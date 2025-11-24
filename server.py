11a3343c9c01f10e3f12752aa9ce1b6dd95828a0
businessvolgger-prog <business.volgger@gmail.com>
2025-11-24 14:52:51 +0100

    Refactor server.py for chatbot functionality

--- server.py
+++ server.py
@@ -69,8 +69,9 @@ def chat():
     answer = completion.choices[0].message.content
     return jsonify({"response": answer})
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
 
 
